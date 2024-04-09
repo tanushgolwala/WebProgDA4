ec6f18505c2e69a22ca492d5ddc8f6fc38e13802
Tanush Golwala <tanushgolwala@gmail.com>
2024-04-09 17:49:25 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Carousel from './components/Carousel';
+import Destinations from './components/Destinations';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Search from './components/Search';
+import Selects from './components/Select';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <Destinations />
+      <Search />
+      <Selects />
+      <Carousel />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
