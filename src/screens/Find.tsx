1c970318705ad5e1dfdc50096226919d4839ef81
Tanush Golwala <tanushgolwala@gmail.com>
2024-04-09 22:10:41 +0530

    feat : loading

--- src/screens/Find.tsx
+++ src/screens/Find.tsx
@@ -2,13 +2,16 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Gallery } from 'react-grid-gallery';
+import { RingLoader } from "react-spinners";
 
 const Find: React.FC = () => {
-    const [images, setImages] = useState([]);
+    const [images, setImages] = useState<any[]>([]);
     const [searchQuery, setSearchQuery] = useState<string>("");
     const location = useLocation();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         const searchParams = new URLSearchParams(location.search);
         const queryParam = searchParams.get("query");
         if (queryParam) {
@@ -32,19 +35,27 @@ const Find: React.FC = () => {
                 caption: photo.photographer
             }));
             setImages(fetchedImages);
+            setLoading(false);
         } catch (error) {
             console.error("Error fetching images:", error);
+            setLoading(false); // Make sure to set loading to false even on error
         }
     };
 
     return (
-        <div className="container mx-auto px-4 py-8 bg-black">
-
-            <div className="flex justify-center items-center">
-                <h1 className="text-3xl font-semibold mb-4 text-white self-center justify-center">{searchQuery}</h1>
+        <div className="container mx-auto px-4 py-8 bg-black min-h-screen min-w-screen">
+            <div className="flex justify-center items-center mb-4">
+                <h1 className="text-3xl font-semibold text-white">{searchQuery}</h1>
             </div>
-            <div className="flex-row justify-center items-center">
-                <Gallery images={images}  />
+            <div className="flex justify-center items-center mb-4">
+                <RingLoader color={"#ffffff"} loading={loading} size={100} />
+            </div>
+            <div className="flex justify-center">
+                {images.length > 0 && 
+                    <div className="max-w-4xl w-full">
+                        <Gallery images={images} />
+                    </div>
+                }
             </div>
         </div>
     );
